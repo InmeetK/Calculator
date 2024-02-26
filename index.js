02852faba3b95147b6c19d3ab7e8df57a91802e4
inmeet <ik822@ic.ac.uk>
2024-02-26 14:56:37 +0000

    More operators implemented.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const readline = require('readline-sync');
 console.log('Welcome to the calculator!');
+console.log('Please enter the operator: ')
+const operator = readline.prompt();
+console.log('\nPlease enter some your first number: ');
+const arg1 = readline.prompt();
+const num1 = +arg1;
+console.log('\nPlease enter some your second number: ');
+const arg2 = readline.prompt();
+const num2 = + arg2;
+
+var result;
+switch (operator) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+}
+console.log("Result: " + result);

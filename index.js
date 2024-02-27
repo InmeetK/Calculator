f6233dc860b2e8cd79d305c69b052cafc4e150b3
inmeet <ik822@ic.ac.uk>
2024-02-27 12:31:17 +0000

    Split code into functions, added checks for user input, and loop after result.

--- index.js
+++ index.js
@@ -1,35 +1,64 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
-
-console.log('Please enter the operator: ')
-const operator = readline.prompt();
-
-console.log('How many numbers do you want to ' + operator + ': ');
-const argCount = readline.prompt();
-const count = +argCount;
-
-let array = new Array(count);
-for (let i = 0; i < count; i++) {
-    console.log('\nPlease enter number ' + (i + 1) + ': ');
-    const argInput = readline.prompt();
-    array[i] = +argInput;
+function printWelcomeMessage() {
+    console.log('\nWelcome to the calculator!');
+    console.log('==========================');
 }
 
-let result = array[0];
-for (let i = 1; i < count; i++) {
-    switch (operator) {
+function performOneCalculation() {
+    console.log('\nPlease enter the operator: ');
+    const operator = readline.prompt();
+
+    const count = printAndGetNum('\nHow many numbers do you want to ' + operator + ': ');
+    const array = getNumberArray(count);
+    const result = calculateResult(array, operator);
+
+    console.log('\nResult: ' + result);
+}
+
+function getNumberArray(count) {
+    let array = new Array(count);
+    for (let i = 0; i < count; i++) {
+        array[i] = printAndGetNum('\nPlease enter number ' + (i + 1) + ': ');
+    }
+    return array;
+}
+
+function calculateResult(array, op) {
+    let count = array.length;
+    let result = array[0];
+    for (let i = 1; i < count; i++) {
+        result = applyOperator(result, array[i], op);
+    }
+    return result;
+}
+
+function printAndGetNum(input) {
+    console.log(input);
+    const promptInput = readline.prompt();
+    const num = +promptInput;
+    if (isNaN(num)) {
+        console.log('\n ||That input was not a number, try again.||')
+        return printAndGetNum(input);
+    }
+    return num;
+}
+
+function applyOperator(arg1, arg2, op) {
+    switch (op) {
         case '+':
-            result += array[i];
-            break;
+            return arg1 + arg2;
         case '-':
-            result -= array[i];
-            break;
+            return arg1 - arg2;
         case '*':
-            result *= array[i];
-            break;
+            return arg1 * arg2;
         case '/':
-            result /= array[i];
+            return arg1 / arg2;
     }
 }
-console.log("Result: " + result);
+
+printWelcomeMessage();
+let again = true;
+while (again) {
+    performOneCalculation();
+}
